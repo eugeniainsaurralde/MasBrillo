370ffd74348cf90e0262c0a049326b414d0b6698
Eugenia Insaurralde <eugeinsaurralde01@gmail.com>
2022-08-29 23:52:20 -0300

    29/8

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  let imagen= "download.jpg" ;
+  let imagen1= "download.jpg" ;
   let titulo= "Big Mac";
   let calorias= "505 kcal";
   let descripcion= "Quizás sean las dos hamburguesas de carne 100% vacuna con esa salsa especial y queso derretido, el toque de cebolla y la frescura de la lechuga o el crocante del pepino, lo que la hace la hamburguesa más famosa del mundo. Un sabor único."
@@ -9,7 +9,7 @@ function App() {
     <div className='container'>
       <div className='row'>
           <div className='col-md-4 offset-md-2'>
-            <img src={imagen} alt= {titulo} className="img-fluid"/>
+            <img src={imagen1} alt= {titulo} className="img-fluid"/>
           </div>
           <div className='col-md-4'>
             <h1>{titulo}</h1>
